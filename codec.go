82739187f60f15009a77c590ff7fa2b1028c5dbb
ZhangGuangxu <zhangxxxxxxx9623@126.com>
2019-11-18 00:36:18 +0800

    :art: modify codec interface

--- codec.go
+++ codec.go
@@ -1,8 +1,20 @@
 package codec
 
-type Codec interface {
-	Decode(data []byte, msg interface{}) error
-	DecodeWithID(msgID int16, data []byte) (msg interface{}, err error)
-	DecodeWithName(msgName int16, data []byte) (msg interface{}, err error)
+type Encoder interface {
 	Encode(msg interface{}) (data []byte, err error)
+}
+
+type Codec interface {
+	Encoder
+	Decode(data []byte, msg interface{}) error
+}
+
+type IDCodec interface {
+	Encoder
+	DecodeWithID(msgID int16, data []byte) (msg interface{}, err error)
+}
+
+type NameCodec interface {
+	Encoder
+	DecodeWithName(msgName int16, data []byte) (msg interface{}, err error)
 }
